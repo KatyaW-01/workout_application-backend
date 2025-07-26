8414e3243c2879cadf3093fea6393e9f86a00fee
Katya Weicht <katya.weicht@gmail.com>
2025-07-26 13:17:50 -0600

    Outline workout flask endpoints for GET, POST, and DELETE

--- server/app.py
+++ server/app.py
@@ -12,5 +12,28 @@ db.init_app(app)
 
 #Define Routes here
 
+@app.route('/workouts', method=["GET"])
+def get_workouts():
+  #list all workouts
+  pass
+
+@app.route('/workouts/<id>', method=["GET"])
+def get_workout(id):
+  #show a single workout with its associated exercises
+  #include reps/sets/duration from WorkoutExercises if you can
+  pass
+
+@app.route('/workouts', methods=["POST"])
+def create_workouts():
+  #create a workout
+  pass
+
+@app.route('/workouts/<id>', methods=["DELETE"])
+def delete_workout(id):
+  #delte a workout
+  #delete associated WorkoutExercises if you can 
+  pass
+
+
 if __name__ == '__main__':
   app.run(port=5555, debug=True)
