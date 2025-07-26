7b6986f19bb479ee8f4dc1914fa0952dc308c8de
Katya Weicht <katya.weicht@gmail.com>
2025-07-26 13:25:41 -0600

    Outline exercise flask endpoints for GET, POST, and DELETE

--- server/app.py
+++ server/app.py
@@ -12,12 +12,12 @@ db.init_app(app)
 
 #Define Routes here
 
-@app.route('/workouts', method=["GET"])
+@app.route('/workouts', methods=["GET"])
 def get_workouts():
   #list all workouts
   pass
 
-@app.route('/workouts/<id>', method=["GET"])
+@app.route('/workouts/<id>', methods=["GET"])
 def get_workout(id):
   #show a single workout with its associated exercises
   #include reps/sets/duration from WorkoutExercises if you can
@@ -32,6 +32,32 @@ def create_workouts():
 def delete_workout(id):
   #delte a workout
   #delete associated WorkoutExercises if you can 
+  pass
+
+@app.route('/exercises', methods=["GET"])
+def get_exercises():
+  #list all exercises
+  pass
+
+@app.route('/exercises/<id>', methods=["GET"])
+def get_exercise(id):
+  #show an exercise and associated workouts
+  pass
+
+@app.route('/exercises', methods=["POST"])
+def create_exercises():
+  #create an exercise
+  pass
+
+@app.route('/exercises/<id>', methods=["DELETE"])
+def delete_exercise(id):
+  #delete an exercse
+  #delete associated WorkoutExercises if you can
+  pass
+
+@app.route('/workouts/<workout_id>/exercises/<exercise_id>/workout_exercises')
+def add_exercise_to_workout(workout_id,exercise_id):
+  #add an exercise to a workout, including reps/sets/duration
   pass
 
 
